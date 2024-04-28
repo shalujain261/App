0192e83f0b46bee452900549221aea46b99967aa
Shalu jain <163709823+shalujain261@users.noreply.github.com>
2024-04-28 05:36:16 -0800

    banana-talk

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-// let username = prompt("Enter your name");
-// alert("My name is " + username);
 
 let btn = document.querySelector(".btn");
 let txtInput = document.querySelector(".text-area");
 let outputClass = document.querySelector(".output");
 
-btn.addEventListener("click",()=>{
-    outputClass.innerText = "Translate it in Banana Language " + txtInput.value;
-})
 
+let ServerUrl = "https://api.funtranslations.com/translate/minion.json?text=Hello%2C%20I%20am%20hungry%21";
+
+function getTranslateUrl(text){
+    return ServerUrl + "?" + "text=" + text;
+}
+
+
+btn.addEventListener("click",()=>{
+    fetch(getTranslateUrl(txtInput.value))
+    .then((response) => (response.json()))
+    .then((json) =>{
+        let translatedText = json.contents.translated;
+        outputClass.innerText = translatedText;
+
+    })
+    .catch((err) => {
+        console.log(err);
+        alert("Something Wrong with server! please try again after some time");
+    })
+});
 
